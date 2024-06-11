fff50853a90af5c62b5080b2b51a7a8787cd743b
LegerLinar <pavelbesedin97@mail.ru>
2024-06-11 21:52:59 +0700

    Executed several wiring methods

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
+    private Needle7 needle;
 
     @Autowired
-    private Needle7 needle;
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
         return ", в яйце иголка " + needle.toString();
     }
+
 }
